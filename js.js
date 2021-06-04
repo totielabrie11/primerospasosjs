d24366a91a1d71de4a6a4667de66f0b1c53c96d7
totielabrie11 <martinezcarlosmanuel.1@gmail.com>
2021-06-04 16:22:32 -0300

    js ultima version

--- js.js
+++ js.js
@@ -4,24 +4,48 @@ let edad = 37;
 let estadoCivil = "soltero"; */
 
 
-let nombre = prompt("ingrese su nombre");
-console.log(nombre);
+//let nombre = prompt("ingrese su nombre");
+//console.log(nombre);
 
-let edad = prompt("ingrese su edad");
-edad = parseInt(edad); //para este caso, me sirve mas ParseInt  Number
-console.log(edad);
+//let edad = prompt("ingrese su edad");
+//edad = parseInt(edad); //para este caso, me sirve mas ParseInt  Number
+//console.log(edad);
 
-let estadoCivil = prompt("ingrese su estaod civil");
-console.log(estadoCivil);
+//let estadoCivil = prompt("ingrese su estaod civil");
+//console.log(estadoCivil);
 
 
-if (edad >= 18 && estadoCivil == ("soltero" || "soltera")) { 
-    alert("Bienvenido " + nombre + " estas autorizado a ingresar a este contenido " + " con el fin de que dejes de ser " + estadoCivil)
-} else if (edad >= 18 && estadoCivil == ("casado" || "casada")) {
-    alert("puedes ingresar " + nombre + " a ver el contenido, bajo tus propios riesgos")
+//if (edad >= 18 && estadoCivil == ("soltero" || "soltera")) { 
+//    alert("Bienvenido " + nombre + " estas autorizado a ingresar a este contenido " + " con el fin de que dejes de ser " + estadoCivil)
+//} else if (edad >= 18 && estadoCivil == ("casado" || "casada")) {
+//    alert("puedes ingresar " + nombre + " a ver el contenido, bajo tus propios riesgos")
+//}
+// else {
+//    alert("hola " + nombre + " No reunes los requisitos para ingresar a este sitio")
+//}
+
+//var amigos = ["carlos", "tito", "pichiman",];
+//var amigos1 = ["totielabrie", "pocha", "angie", "soledad"];
+//var amigos2 = amigos.concat(amigos1); // para concatenar una cadena de texto de un arraid , asignara un valor por unidad
+    //amigos2.push("aaron", "loco", "ezequiel");
+    
+    //document.write("Estos son todos tus amigos: " + amigos2.join(" / ") + " que suman un total de " + amigos2.length + " amigos");
+    //document.write("Estos son todos tus amigos " + amigos2 + " que suman un total de " + amigos2.length + " amigos");
+    //amigos2.join() le agrega un separador entre cada elemento
+    //amigos2.lenght suma la cantidad de elementos y devuelve el valor = cantidad
+/* var edad = 20
+var DiasDeSemana = ["lunes", "martes", "miercoles", "jueves", "viernes", "sabado", "domingo"]
+DiasDeSemana.length
+console.log(DiasDeSemana)
+
+for ( i = 0; i <= DiasDeSemana.length - 1; i++ ) {    //ciclo for
+    document.write( DiasDeSemana[i] + "<br><br>");
+} */
+var DiasDeSemana = ["lunes", "martes", "miercoles", "jueves", "viernes", "sabado", "domingo"];
+var NumeroDias = DiasDeSemana.length - 1;
+var i = 0;
+
+while (i <= NumeroDias) {
+    document.write( DiasDeSemana[i] + "<br><br>");   //ciclo while
+    i++;
 }
- else {
-    alert("hola " + nombre + " No reunes los requisitos para ingresar a este sitio")
-}
-
-
